ae1e04b6ede9bec1881a0176cb6290871acc3e92
emilljung <emil_ljung1991@hotmail.com>
2017-07-17 23:40:39 +0200

    Update Person.lua

--- Bin/x86/Person.lua
+++ Bin/x86/Person.lua
@@ -1,5 +1,7 @@
 Person = {}
 
+-- Parent class to Leader, Rager & CoffeeConsumer. All the sub classes have access to these functions. 
+
 function Person:new(p_name, p_room)
     print(p_name .. " is starting in " .. p_room:GetRoomName())
 
